a190e4f01a1515d3bac6ddc9b265e8a17f6b8dbf
subham <freelancerrsubham@gmail.com>
2023-07-16 09:38:24 +0545

    structure configuration

--- server/src/index.js
+++ server/src/index.js
@@ -1,22 +1,12 @@
 const express = require('express')
-const mongoose = require('mongoose');
- 
+require('dotenv').config()
+const connection = require('./dbConfig/connection')
+const products = require('./models/products')
+connection()
+
 const app = express()
-const port = 4000
 app.use(express.json())
-const { Schema } = mongoose;
-mongoose.connect('mongodb://localhost:27017/Ecommerce');
- 
-const productSchema = new Schema({
- productName: String, // String is shorthand for {type: String}
- productPrice: Number,
- description: String,
- image: String,
- category: String,
- flag: {type: String, default:'valid'}
-});
- 
-const Products = mongoose.model('Product', productSchema);
+
  
 app.post('/products', (req, res) => {
  Products.create(req.body)
@@ -41,6 +31,6 @@ app.put('/products/:id', async(req, res) => {
  const data = await Products.find()
  })
 
- app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`)
+ app.listen(process.env.PORT, () => {
+    console.log(`Example app listening on port ${process.env.PORT}`)
   })
